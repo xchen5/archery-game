f366164813ed65828beb593ec8320c7fc89bb397
xchen5 <xchen269@binghamton.edu>
2021-11-03 10:32:50 -0400

    added files for future classes as well as bow sprite

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,7 +8,7 @@ void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft])
       .then((_) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [])
-        .then((value) => runApp(MyApp()));
+        .then((value) => runApp(const MyApp()));
   });
 }
 
@@ -37,16 +39,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,52 +46,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget buildTarget() {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 2,
+  Widget buildTarget(double height, double width) {
+    Random r = Random();
+    //TODO Change from hardcoded Coordinates
+    double randomDouble = r.nextDouble() * 200;
+    print(randomDouble);
+    return Positioned(
+        child: Image.asset(
+          'assets/images/target-sprite.png',
+          height: height * 0.23,
+          width: width * 0.14,
         ),
-      ),
+        //TODO Change from hardcoded Coordinates
+        left: 500,
+        right: 100,
+        top: randomDouble);
+  }
+
+  Widget buildBow(double height, double width) {
+    return Positioned(
       child: Image.asset(
-        'assets/images/target-sprite.png',
-        height: 131,
-        width: 479,
+        'assets/images/bow-sprite.png',
+        height: height * 0.23,
+        width: width * 0.14,
       ),
+      //TODO Change from hardcoded Coordinates
+      top: 200,
+      left: 75,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
+    print(screenWidth);
+    print(screenHeight);
     return Scaffold(
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[buildTarget()],
-        ),
-      ),
-    );
+        backgroundColor: Colors.grey,
+        body: Container(
+          child: Stack(
+            children: [
+              Stack(
+                children: [buildTarget(screenHeight, screenWidth)],
+              ),
+              buildBow(screenHeight, screenWidth)
+            ],
+          ),
+        ));
   }
 }
